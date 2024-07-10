aa00e353b7a4d2868989567117fd7bd4fffe2bd8
eloramaysa <eloramaysa@gmail.com>
2024-07-09 21:19:04 -0300

    Create data class

---
+++ BudgeBuddyProject/Models/Data/AuthData.cs
@@ -0,0 +1,11 @@
+﻿namespace BudgeBuddyProject.Models.Data
+{
+    public class AuthData
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string CreatedBy { get; set; } = string.Empty;
+        public DateTime UpdatedDate { get; set; }
+        public string UpdatedBy { get; set; } = string.Empty;
+    }
+}
